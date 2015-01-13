10c6b721c0762ee4eface0b1c476d52f8de5c57a
Jason Ish <ish@unx.ca>
2015-01-13 10:27:25 -0600

    Colourize the JSON view of the event (issue #4).

--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -62,6 +62,39 @@
 
     }]);
 
+    /**
+     * Colourize a JSON string.
+     *
+     * Based on code snippet from:
+     * http://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
+     */
+    angular.module("app").filter("colourizeJson", ["$sce", function($sce) {
+
+        return function(json) {
+
+            json = json.replace(/&/g, '&').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+            var colourized = json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+                var cls = 'number';
+                if (/^"/.test(match)) {
+                    if (/:$/.test(match)) {
+                        cls = 'key';
+                    } else {
+                        cls = 'string';
+                    }
+                } else if (/true|false/.test(match)) {
+                    cls = 'boolean';
+                } else if (/null/.test(match)) {
+                    cls = 'null';
+                }
+                return '<span class="' + cls + '">' + match + '</span>';
+            });
+
+            return $sce.trustAsHtml(colourized);
+
+        };
+
+    }]);
+
     angular.module("app").filter("formatEventDescription",
         ["$sce", "Util", function ($sce, Util) {
 
