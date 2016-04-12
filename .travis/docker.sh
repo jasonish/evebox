f1817845d4931b68332c912dc5dfcc852c122871
Jason Ish <ish@unx.ca>
2016-04-12 16:04:54 -0600

    travis: only do docker deploy for specific repo/branch

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -3,6 +3,23 @@
 set -e
 set -x
 
+if [ -e _docker_done ]; then
+    echo "After deploy already done, exiting."
+    exit 0
+fi
+
+if [ "${TRAVIS_REPO_SLUG}" != "${DEPLOY_REPO}" ]; then
+    echo "docker: not deploying for repo ${TRAVIS_REPO_SLUG}."
+    exit 0
+fi
+
+if [ "${TRAVIS_BRANCH}" != "${DEPLOY_BRANCH}" ]; then
+    echo "docker: not deploying for branch ${TRAVIS_BRANCH}."
+    exit 0
+fi
+
+touch _docker_done
+
 echo "Triggering build on Docker hub."
 curl -v -H "Content-Type: application/json" \
      --data '{"build": true}' \
