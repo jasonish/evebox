bb2950e85b772e4f65934e1dc66c3a02086f49f0
Jason Ish <ish@unx.ca>
2016-04-12 12:39:40 -0600

    travis: can I get some output from the docker deploy?

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 
 set -e
+set -x
 
 echo "Triggering build on Docker hub."
-curl -H "Content-Type: application/json" \
+curl -v -H "Content-Type: application/json" \
      --data '{"build": true}' \
      -X POST https://registry.hub.docker.com/u/jasonish/evebox/trigger/${DOCKER_TRIGGER_TOKEN}/
