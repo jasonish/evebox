7e3cbac72a47a0eb8a28c808011654a4f95a3519
Jason Ish <ish@unx.ca>
2016-04-13 22:41:28 -0600

    travis: more deployment cleanup

--- .travis/deploy-packages.sh
+++ .travis/deploy-packages.sh
@@ -20,20 +20,26 @@ fi
 
 # Deploy zip's to Bintray.
 for zip in dist/evebox-*.zip; do
+    echo "Uploading ${zip}."
     curl -T ${zip} -u jasonish:${BINTRAY_API_KEY} \
 	 "https://api.bintray.com/content/jasonish/evebox-zip-dev/evebox/dev/$(basename ${zip})?publish=1&override=1"	 
+    echo
 done
 
 # Deploy RPM to Bintray.
-for rpm in dist/evebox-*.rpm; do
+for rpm in dist/evebox*.rpm; do
+    echo "Uploading ${rpm}."
     curl -T ${rpm} -u jasonish:${BINTRAY_API_KEY} \
-	 "https://api.bintray.com/content/jasonish/evebox-rpm-current/evebox/current/$(basename ${rpm})?publish=1&override=1"
+	 "https://api.bintray.com/content/jasonish/evebox-rpm-dev/evebox/dev/$(basename ${rpm})?publish=1&override=1"
+    echo
     break
 done
 
 # Deploy Debian package to Bintray.
-for deb in dist/evebox-*.deb; do
-    curl ${deb} -u jasonish:${BINTRAY_API_KEY} \
-	 "https://api.bintray.com/content/jasonish/deb-evebox-latest/evebox/latest/$(basename ${deb})?publish=1&override=1"
+for deb in dist/evebox*.deb; do
+    echo "Uploading ${deb}."
+    curl -T ${deb} -u jasonish:${BINTRAY_API_KEY} \
+	 "https://api.bintray.com/content/jasonish/deb-evebox-latest/evebox/latest/$(basename ${deb});deb_distribution=jessie;deb_component=main;deb_architecture=amd64?publish=1&override=1"
+    echo
     break
 done
