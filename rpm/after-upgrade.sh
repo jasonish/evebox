7af31b18248294b2f5c2a76a779ed098e40b3d25
Jason Ish <ish@unx.ca>
2017-02-17 17:05:20 -0600

    rpm: restart agent after install (if running)

--- rpm/after-upgrade.sh
+++ rpm/after-upgrade.sh
@@ -6,5 +6,12 @@ set -e
 
 # Restart evebox if running.
 if /bin/systemctl status evebox > /dev/null; then
+    echo "Restarting evebox."
     /bin/systemctl restart evebox
+fi
+
+# Restart evebox-agent if running.
+if /bin/systemctl status evebox-agent > /dev/null; then
+    echo "Restarting evebox-agent."
+    /bin/systemctl restart evebox-agent
 fi
