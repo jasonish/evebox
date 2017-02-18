120a8f55b67d8647f1d868e501a6c34d263f5eec
Jason Ish <ish@unx.ca>
2017-02-18 11:51:54 -0600

    sqlite: more indexes

--- resources/sqlite/V1.sql
+++ resources/sqlite/V1.sql
@@ -6,14 +6,22 @@ CREATE TABLE events (
   source    JSON
 );
 
--- Index alert.signature
--- - Inbox and alert views group by alert.signature.
---
+CREATE INDEX events_timestamp ON events (timestamp);
+
+CREATE INDEX events_archived ON events (archived);
+
+CREATE INDEX events_event_type
+  ON events (json_extract(source, '$.event_type'));
+
+CREATE INDEX events_src_ip
+  ON events (json_extract(source, '$.src_ip'));
+
+CREATE INDEX events_dest_ip
+  ON events (json_extract(source, '$.dest_ip'));
+
 CREATE INDEX events_alert_signature_index
   ON events (json_extract(source, '$.alert.signature'));
 
--- Index alert.signature_id
--- - Speeds up archiving where signature_id field in the where.
 CREATE INDEX events_alert_signature_id_index
   ON events (json_extract(source, '$.alert.signature_id'));
 
