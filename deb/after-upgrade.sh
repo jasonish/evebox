a76f85f3851a4209db7f2b3fc8964769074f13ff
Jason Ish <ish@unx.ca>
2017-03-16 12:06:51 -0600

    systemd: only attempt restart of daemon-reload succeeds

--- deb/after-upgrade.sh
+++ deb/after-upgrade.sh
@@ -2,7 +2,13 @@
 
 set -e
 
+if ! /bin/systemctl daemon-reload > /dev/null 2>&1; then
+    # Exit now if this failed. May be running in a container.
+    exit 0
+fi
+
 # Restart evebox if running.
-if systemctl status evebox > /dev/null; then
-    systemctl restart evebox
+if /bin/systemctl status evebox > /dev/null; then
+    echo "Restarting evebox."
+    /bin/systemctl restart evebox
 fi
--- rpm/after-upgrade.sh
+++ rpm/after-upgrade.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-/bin/systemctl daemon-reload
+if ! /bin/systemctl daemon-reload > /dev/null 2>&1; then
+    # Exit now if this failed. May be running in a container.
+    exit 0
+fi
 
 # Restart evebox if running.
 if /bin/systemctl status evebox > /dev/null; then
