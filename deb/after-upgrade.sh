a08f401e45089dc6238df6173748ac94e993d987
Jason Ish <ish@unx.ca>
2017-04-20 23:25:54 -0600

    deb: restart evebox-agent after upgrade

--- deb/after-upgrade.sh
+++ deb/after-upgrade.sh
@@ -24,3 +24,9 @@ if /bin/systemctl status evebox > /dev/null; then
     echo "Restarting evebox."
     /bin/systemctl restart evebox
 fi
+
+# Restart evebox-agent if running.
+if /bin/systemctl status evebox-agent > /dev/null; then
+    echo "Restarting evebox-agent."
+    /bin/systemctl restart evebox-agent
+fi
