ad0f10d9eab454d0714f9a347751d54af634cdc7
Jason Ish <ish@unx.ca>
2017-04-22 23:54:11 -0600

    docker debian test installers

---
+++ docker/debian-8-unstable-test-install/entry.sh
@@ -0,0 +1,12 @@
+# /bin/sh
+
+set -e
+
+apt-get -y update
+apt-get -y install wget
+wget -qO - https://evebox.org/files/GPG-KEY-evebox | apt-key add -
+echo "deb http://files.evebox.org/evebox/debian unstable main" | \
+    tee /etc/apt/sources.list.d/evebox.list
+apt-get -y update
+apt-get -y install evebox
+evebox version
