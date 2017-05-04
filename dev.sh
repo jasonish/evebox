82e8df7875880a8e10e26dbc1ca4774b4d89597a
Jason Ish <ish@unx.ca>
2017-05-04 01:37:53 -0600

    dev.sh - a simpler script for running in dev mode

---
+++ dev.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+set -x
+set -e
+
+trap 'echo "Killing background jobs..."; kill $(jobs -p)' EXIT
+
+(cd webapp && make serve) &
+
+reflex -s -R bindata\.go -r \.go$ -- \
+       sh -c "NO_WEBAPP=1 make evebox && \
+                 ./evebox server --dev http://localhost:4200 $@"
