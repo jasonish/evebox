ea90e25ea8a97ef8c02d7055e88764b13f82e1fe
Jason Ish <ish@unx.ca>
2017-05-12 17:06:14 -0600

    dev.sh - fix argument handling

--- dev.sh
+++ dev.sh
@@ -5,8 +5,10 @@ set -e
 
 trap 'echo "Killing background jobs..."; kill $(jobs -p)' EXIT
 
+args="$@"
+
 (cd webapp && make serve) &
 
 reflex -s -R bindata\.go -r \.go$ -- \
        sh -c "NO_WEBAPP=1 make evebox && \
-                 ./evebox server --dev http://localhost:4200 $@"
+                 ./evebox server --dev http://localhost:4200 ${args}"
