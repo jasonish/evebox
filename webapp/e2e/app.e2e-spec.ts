c29f9f46d883b65e835b6af35d15db91fc19b046
Jason Ish <ish@unx.ca>
2017-09-26 07:45:37 +0100

    webapp: sync up with angular cli

--- webapp/e2e/app.e2e-spec.ts
+++ webapp/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { WebappPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('webapp App', function() {
-  let page: WebappPage;
+describe('ngapp App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new WebappPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
