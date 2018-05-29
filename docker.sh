04a6dc60c484341cc79d97d57f2f27edf1eace34
Jason Ish <ish@unx.ca>
2018-05-29 08:50:03 -0600

    docker release: update node and golang

--- docker.sh
+++ docker.sh
@@ -1,26 +1,5 @@
 #! /bin/sh
 
-docker_build() {
-    docker build --rm -t evebox/builder - < Dockerfile
-}
-
-docker_run() {
-    docker run --rm -it \
-	   -v $(pwd):/go/src/github.com/jasonish/evebox \
-	   -w /go/src/github.com/jasonish/evebox \
-	   -e WITH_SQLITE=${WITH_SQLITE} \
-	   --privileged \
-	   evebox/builder $@
-}
-
-needs_privilege() {
-    if [ "$(getenforce || true)" = "Enforcing" ]; then
-	return 0
-    else
-	return 1
-    fi
-}
-
 release() {
     privileged=""
     if needs_privilege; then
@@ -46,30 +25,12 @@ case "$1" in
 	release
 	;;
 
-    install-deps)
-	docker_build
-	docker_run make install-deps
-	;;
-
-    build)
-	docker_build
-	docker_run make
-	;;
-
-    shell)
-	docker_build
-	docker_run
-	;;
-
     *)
 	cat <<EOF
 usage: ./docker.sh <command>
 
 Commands:
     release            Build a release
-    install-deps       Install deps (make install-deps)
-    build              Build evebox (make)
-    shell              Execute shell
 EOF
 	;;
 
