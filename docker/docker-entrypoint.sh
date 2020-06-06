ef17282d8a0785bfaa02d016e9acf8b58b6504e0
Jason Ish <ish@unx.ca>
2020-06-06 00:13:38 -0600

    docker: bind to 0.0.0.0 by default
    In the default Docker image, bind to 0.0.0.0 as a convenience.
    
    In most use cases, the user will need to expose the port anyways.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,27 +2,15 @@
 
 set -e
 
+# Open up the bind host to 0.0.0.0 as we are running in a container.
+if [ "${EVEBOX_HTTP_HOST}" = "" ]; then
+    EVEBOX_HTTP_HOST=0.0.0.0
+    export EVEBOX_HTTP_HOST
+fi
+
 # Add evebox as command if needed
 if [ "${1:0:1}" = "-" ]; then
     set -- evebox server "$@"
-fi
-
-if [ "$1" = "evebox" ]; then
-    if [ "$ELASTICSEARCH_URL" -o "$ELASTICSEARCH_PORT_9200_TCP" ]; then
-	: ${ELASTICSEARCH_URL:=http://elasticsearch:9200}
-	export ELASTICSEARCH_URL
-    else
-	cat >&2 <<EOF
-warning: ELASTICSEARCH_URL not set, possible solutions:
-   1. Link to an elasticsearch container with:
-         docker run --link some-elasticsearch:elasticsearch ...
-   2. Set the ELASTICSEARCH_URL like:
-         docker run -e ELASTICSEARCH_URL=http://192.168.1.100:9200 ...
-   3. Provide the Elastic Search URL on the evebox command line:
-         docker run ... jasonish/evebox -e http://192.168.1.100:9200
-EOF
-    fi
-
 fi
 
 exec "$@"
