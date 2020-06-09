8962b32672dbfbc44df75e40dfaecac952ae37bd
Jason Ish <ish@unx.ca>
2020-06-09 17:14:39 -0600

    dev.sh: install webapp deps if needed
    Also remove evebox.sh which wasn't that useful.

--- dev.sh
+++ dev.sh
@@ -7,5 +7,9 @@ trap 'echo "Killing background jobs..."; kill $(jobs -p)' EXIT
 
 args="$@"
 
+if ! test -d webapp/node_modules; then
+    (cd webapp && make install-deps)
+fi
+
 (cd webapp && npm start) &
 cargo watch -i webapp -x "run server ${args}"
