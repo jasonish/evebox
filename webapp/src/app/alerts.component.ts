57f652e732ce582a43cbe839d437b682dd1ec976
Jason Ish <ish@unx.ca>
2020-09-03 23:57:24 -0600

    webapp/alerts: handle case where alert has no tags
    Not a usual occurrence, but if the agent doesn't add a tags,
    the tags list may not exist. So make sure it exists before
    adding to it.

--- webapp/src/app/alerts.component.ts
+++ webapp/src/app/alerts.component.ts
@@ -1,28 +1,17 @@
-/* Copyright (c) 2014-2016 Jason Ish
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+// Copyright (C) 2014-2020 Jason Ish
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import {AlertService} from "./alert.service";
 import {AfterViewChecked, Component, OnDestroy, OnInit} from "@angular/core";
@@ -451,6 +440,7 @@ export class AlertsComponent implements OnInit, OnDestroy, AfterViewChecked {
     archiveSelected() {
         let selected = this.rows.filter((row: any) => {
             return row.selected &&
+                row.event.event._source.tags &&
                 row.event.event._source.tags.indexOf("archived") < 0;
         });
         selected.forEach((row: any) => {
@@ -465,6 +455,9 @@ export class AlertsComponent implements OnInit, OnDestroy, AfterViewChecked {
         }
 
         // Optimistically mark the event as archived.
+        if (!row.event.event._source.tags) {
+            row.event.event._source.tags = [];
+        }
         row.event.event._source.tags.push("archived");
 
         // If in inbox, also remove it from view.
