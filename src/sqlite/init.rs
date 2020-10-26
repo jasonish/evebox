2b2a8cbe9e8c3e606e2d95d85ebfcbf310eaba00
Jason Ish <ish@unx.ca>
2020-10-26 16:45:49 -0600

    sqlite: fix migrations
    Latest revision was always being overwritten.

--- src/sqlite/init.rs
+++ src/sqlite/init.rs
@@ -18,34 +18,39 @@ use crate::resource::Resource;
 use rusqlite::params;
 
 pub fn init_db(db: &mut rusqlite::Connection, prefix: &str) -> Result<(), rusqlite::Error> {
-    let mut version = db
+    let version = db
         .query_row("select max(version) from schema", params![], |row| {
             let version: i64 = row.get(0).unwrap();
             Ok(version)
         })
-        .unwrap_or(0);
+        .unwrap_or(-1);
+    let mut next_version = version + 1;
 
     loop {
-        let filename = format!("{}/V{}.sql", prefix, version);
+        let filename = format!("{}/V{}.sql", prefix, next_version);
         let asset = Resource::get(&filename);
         if let Some(asset) = asset {
             if version == 0 {
                 log::info!("Initializing SQLite database")
             } else {
-                log::info!("Updating SQLite database to schema version {}", version);
+                log::info!(
+                    prefix = prefix,
+                    "Updating SQLite database to schema version {}",
+                    version
+                );
             }
             let asset = String::from_utf8_lossy(&asset);
             let tx = db.transaction()?;
             tx.execute_batch(&asset)?;
             tx.execute(
                 "INSERT INTO schema (version, timestamp) VALUES (?1, date('now'))",
-                params![version],
+                params![next_version],
             )?;
             tx.commit()?;
         } else {
             break;
         }
-        version += 1;
+        next_version += 1;
     }
 
     Ok(())
