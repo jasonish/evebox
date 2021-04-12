aa9bc0f33a9402b4862108904774a338974eb545
Jason Ish <ish@unx.ca>
2021-04-12 17:07:17 -0600

    doc: use readthedocs theme

--- doc/dev.sh
+++ doc/dev.sh
@@ -1,13 +1,3 @@
-#! /usr/bin/env bash
+#! /bin/sh
 
-# Kill background jobs on exit.
-trap 'kill $(jobs -p)' EXIT
-
-# Do an initial make so the output directory exists.
-make html
-
-# Start reflex and save its pid.
-reflex -- make html &
-
-# Now start devd.
-devd --open -l _build/html
+sphinx-autobuild . _build/html
