f0fb673c5a018f65bf38026b7ef1e4f25c19d3e4
Jason Ish <ish@unx.ca>
2022-05-04 12:26:30 -0600

    webapp/events: fix protocol badge
    bootstrap migration issue

--- webapp/src/app/event-table.component.ts
+++ webapp/src/app/event-table.component.ts
@@ -1,28 +1,6 @@
-/* Copyright (c) 2014-2021 Jason Ish
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+// Copyright (C) 2014-2022 Jason Ish <jason@codemonkey.net>
+//
+// SPDX-License-Identifier: MIT
 
 import { Component, Input, OnDestroy, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
@@ -76,7 +54,7 @@ import { indexOf } from "./utils";
                         </td>
                         <td style="word-break: break-all;">
                             {{row | eveboxEventDescriptionPrinter}}
-                            <span class="badge badge-secondary"
+                            <span class="badge bg-secondary"
                                   *ngIf="row._source.app_proto && row._source.app_proto != 'failed'">
                                 {{row._source.app_proto}}
                             </span>
