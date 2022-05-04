d84b12b041da54cdf1ed556f970e7139380ba343
Jason Ish <ish@unx.ca>
2022-05-04 17:15:54 -0600

    webapp/alerts: try a less disruptive load animation

--- webapp/src/app/alerts.component.ts
+++ webapp/src/app/alerts.component.ts
@@ -1,22 +1,6 @@
-// Copyright (C) 2014-2020 Jason Ish
+// Copyright (C) 2014-2022 Jason Ish <jason@codemonkey.net>
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
-// OR OTHER DEALINGS IN THE SOFTWARE.
+// SPDX-License-Identifier: MIT
 
 import { AlertService } from "./alert.service";
 import { AfterViewChecked, Component, OnDestroy, OnInit } from "@angular/core";
@@ -52,9 +36,6 @@ const DEFAULT_SORT_BY = "timestamp";
 
 @Component({
     templateUrl: "./alerts.component.html",
-    animations: [
-        loadingAnimation,
-    ]
 })
 export class AlertsComponent implements OnInit, OnDestroy, AfterViewChecked {
 
