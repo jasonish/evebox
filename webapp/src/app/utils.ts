9ab54eb3b30cf752c2d4bcd7ed724493b256b9fc
Jason Ish <ish@unx.ca>
2023-01-12 12:50:39 -0600

    webapp: remove angular frontend

--- webapp/src/app/utils.ts
+++
@@ -1,7 +0,0 @@
-export function indexOf(array: any, what: any): number {
-  if (array && Array.isArray(array)) {
-    return array.indexOf(what);
-  } else {
-    return -1;
-  }
-}
--- webapp/src/environments/rev.ts
+++
@@ -1,1 +0,0 @@
-export const REV = "5c106d5";
