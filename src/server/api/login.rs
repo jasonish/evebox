93a3c24c71cc97ec64345570e1887cdcb4a4c8e0
Jason Ish <ish@unx.ca>
2023-01-12 11:29:33 -0600

    server: login, remove login types array
    Instead using a scaler with a single login type.

--- src/server/api/login.rs
+++ src/server/api/login.rs
@@ -1,23 +1,6 @@
-// Copyright (C) 2020 Jason Ish
+// SPDX-FileCopyrightText: (C) 2020 Jason Ish <jason@codemonkey.net>
 //
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-//
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// SPDX-License-Identifier: MIT
 
 use crate::prelude::*;
 use axum::extract::Extension;
@@ -45,7 +28,7 @@ pub(crate) async fn options(
     let response = json!({
         "authentication": {
             "required": context.config.authentication_required,
-            "types": [context.config.authentication_type.to_string()],
+            "type": context.config.authentication_type.to_string(),
         }
     });
     Json(response)
