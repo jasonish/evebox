bc5310253addb1bb0b717aa08f28d0bda99be505
Jason Ish <ish@unx.ca>
2023-02-18 18:48:22 -0600

    sqlite: fix typo in migration

--- resources/sqlite/migrations/V4__EventsSourceValues.sql
+++ resources/sqlite/migrations/V4__EventsSourceValues.sql
@@ -1,5 +1,5 @@
 -- A textual column that will only contain values extracted from the
 -- JSON without field names.  Known base64 values will not be
--- included. This is that data that will be indexed with full text
+-- included. This is the data that will be indexed with full text
 -- search.
 ALTER TABLE events ADD source_values TEXT;
