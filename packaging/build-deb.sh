ed06fce5362f16e49f84dbe46e6e11943232b1c5
Jason Ish <ish@unx.ca>
2023-02-20 12:13:17 -0600

    packaging/debian: simplify a little
    No need for a /etc/default file, but still allow a user to create one
    if needed.

--- packaging/build-deb.sh
+++ packaging/build-deb.sh
@@ -1,27 +1,23 @@
 #! /bin/sh
 
 set -e
+set -x
 
 CARGO_VERSION=$(cat Cargo.toml | awk '/^version/ { gsub(/"/, "", $3); print $3 }')
 VERSION=$(echo ${CARGO_VERSION} | sed 's/\(.*\)\-.*/\1/')
 VERSION_SUFFIX=$(echo ${CARGO_VERSION} | sed -n 's/.*-\(.*\)/\1/p')
 DATE=$(date +%s)
 
-ARGS=()
-
 case "${1}" in
     "x86_64"|"amd64")
-	ARGS+=("-a" "amd64")
 	ARCH="amd64"
 	BIN="./dist/evebox-latest-linux-x64/evebox"
 	;;
     "arm64")
-	ARGS+=("-a" "arm64")
 	ARCH="arm64"
 	BIN="./dist/evebox-latest-linux-arm64/evebox"
 	;;
     "arm")
-	ARGS+=("-a" "armhf")
 	ARCH="armhf"
 	BIN="./dist/evebox-latest-linux-arm/evebox"
 	;;
@@ -40,19 +36,14 @@ else
     FILENAME="$evebox-${VERSION}-${ARCH}.deb"
 fi
 
-fpm -t deb -n evebox -s dir --epoch 1 "${ARGS[@]}" \
+fpm --verbose -t deb -n evebox -s dir --epoch 1 \
     -a "${ARCH}" \
     -p "dist/${FILENAME}" -v "${VERSION}" \
     --force \
     --after-install=./packaging/debian/after-install.sh \
     --after-upgrade=./packaging/debian/after-upgrade.sh \
-    --deb-no-default-config-files \
-    --config-files=/etc/default/evebox \
-    --config-files=/etc/evebox/evebox.yaml \
+    --config-files /etc/evebox/evebox.yaml \
     "${BIN}"=/usr/bin/evebox \
-    ./examples/evebox.yaml=/etc/evebox/evebox.yaml.example \
-    ./examples/agent.yaml=/etc/evebox/agent.yaml.example \
-    ./packaging/evebox.yaml=/etc/evebox/evebox.yaml \
-    ./packaging/debian/evebox.default=/etc/default/evebox \
     ./packaging/debian/evebox.service=/lib/systemd/system/evebox.service \
-    ./packaging/debian/evebox-agent.service=/lib/systemd/system/evebox-agent.service
+    ./packaging/debian/evebox-agent.service=/lib/systemd/system/evebox-agent.service \
+    ./packaging/evebox.yaml=/etc/evebox/
--- packaging/debian/after-upgrade.sh
+++ packaging/debian/after-upgrade.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 set -e
+set -x
 
 USERNAME=evebox
 HOMEDIR=/var/lib/evebox
