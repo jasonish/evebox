435b3418391e47f4f079d47e9d03b453daabb8d9
Jason Ish <jason@codemonkey.net>
2023-09-04 23:34:35 -0600

    config/users: use struct based command line parser

--- src/commands/config/users.rs
+++ src/commands/config/users.rs
@@ -1,74 +1,76 @@
-// Copyright (C) 2020 Jason Ish
-//
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-//
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// SPDX-FileCopyrightText: (C) 2020 Jason Ish <jason@codemonkey.net>
+// SPDX-License-Identifier: MIT
 
 use std::io::{stdin, stdout, Write};
 use std::path::PathBuf;
 
 use anyhow::Result;
-use clap::Arg;
-use clap::Command;
+use clap::CommandFactory;
+use clap::FromArgMatches;
+use clap::Parser;
+use clap::Subcommand;
 
 use crate::sqlite::configrepo::ConfigRepo;
 
-pub fn users_subcommand() -> clap::Command {
-    clap::Command::new("users")
-        .subcommand(Command::new("list").alias("ls").about("List users"))
-        .subcommand(
-            Command::new("add")
-                .about("Add user")
-                .arg(
-                    Arg::new("username")
-                        .long("username")
-                        .short('u')
-                        .value_name("USERNAME")
-                        .help("Username"),
-                )
-                .arg(
-                    Arg::new("password")
-                        .long("password")
-                        .short('p')
-                        .value_name("PASSWORD")
-                        .help("Password"),
-                ),
-        )
-        .subcommand(
-            Command::new("rm")
-                .about("Remove user")
-                .arg(Arg::new("username").required(true)),
-        )
-        .subcommand(
-            Command::new("passwd")
-                .alias("password")
-                .about("Change password for user")
-                .arg(Arg::new("username").required(true)),
-        )
+#[derive(Parser, Debug)]
+#[command(name = "users", about = "Configure users")]
+pub(crate) struct UsersCommand {
+    #[command(subcommand)]
+    command: UsersCommands,
 }
 
-pub fn main(args: &clap::ArgMatches) -> Result<()> {
-    match args.subcommand() {
-        Some(("list", args)) => list(args),
-        Some(("add", args)) => add(args),
-        Some(("rm", args)) => remove(args),
-        Some(("passwd", args)) => password(args),
-        _ => Err(anyhow!("config users: no subcommand provided")),
+#[derive(Debug, Subcommand)]
+enum UsersCommands {
+    /// Add a new user
+    Add(AddArgs),
+    /// Remove an existing user
+    Rm {
+        username: String,
+        #[arg(from_global, id = "data-directory")]
+        data_directory: Option<String>,
+    },
+    /// List users
+    #[command(alias = "ls")]
+    List {
+        #[arg(from_global, id = "data-directory")]
+        data_directory: Option<String>,
+    },
+    /// Change password
+    Passwd {
+        username: String,
+        #[arg(from_global, id = "data-directory")]
+        data_directory: Option<String>,
+    },
+}
+
+#[derive(Parser, Debug)]
+struct AddArgs {
+    #[arg(long, short)]
+    username: Option<String>,
+    #[arg(long, short)]
+    password: Option<String>,
+
+    #[arg(from_global, id = "data-directory")]
+    data_directory: Option<String>,
+}
+
+pub(crate) fn command() -> clap::Command {
+    UsersCommand::command()
+}
+
+pub(crate) fn main(args: &clap::ArgMatches) -> Result<()> {
+    let args = UsersCommands::from_arg_matches(args)?;
+    match args {
+        UsersCommands::Add(args) => add(args),
+        UsersCommands::List { data_directory } => list(data_directory),
+        UsersCommands::Rm {
+            username,
+            data_directory,
+        } => remove(username, data_directory),
+        UsersCommands::Passwd {
+            username,
+            data_directory,
+        } => password(username, data_directory),
     }
 }
 
@@ -82,8 +84,8 @@ fn open_config_repo(data_directory: Option<&str>) -> Result<ConfigRepo> {
     Ok(repo)
 }
 
-fn list(args: &clap::ArgMatches) -> Result<()> {
-    let repo = open_config_repo(args.get_one::<String>("data-directory").map(|s| &**s))?;
+fn list(dir: Option<String>) -> Result<()> {
+    let repo = open_config_repo(dir.as_deref())?;
     let users = repo.get_users()?;
     for user in users {
         println!("{}", serde_json::to_string(&user).unwrap());
@@ -99,10 +101,10 @@ fn get_input(prompt: &str) -> Result<String> {
     Ok(input.trim().to_string())
 }
 
-fn add(args: &clap::ArgMatches) -> Result<()> {
-    let repo = open_config_repo(args.get_one::<String>("data-directory").map(|s| &**s))?;
+fn add(args: AddArgs) -> Result<()> {
+    let repo = open_config_repo(args.data_directory.as_deref())?;
 
-    let username = if let Some(username) = args.get_one::<String>("username") {
+    let username = if let Some(username) = args.username {
         username.to_string()
     } else {
         get_input("Username: ")?
@@ -111,7 +113,7 @@ fn add(args: &clap::ArgMatches) -> Result<()> {
         return Err(anyhow!("empty username not allowed"));
     }
 
-    let password = if let Some(password) = args.get_one::<String>("password") {
+    let password = if let Some(password) = args.password {
         password.to_string()
     } else {
         // Yes, we're allowing empty passwords.
@@ -125,23 +127,22 @@ fn add(args: &clap::ArgMatches) -> Result<()> {
 
     repo.add_user(&username, &password)?;
     println!("User added: username=\"{username}\"");
+
     Ok(())
 }
 
-fn remove(args: &clap::ArgMatches) -> Result<()> {
-    let repo = open_config_repo(args.get_one::<String>("data-directory").map(|s| &**s))?;
-    let username = args.get_one::<String>("username").unwrap();
-    if repo.remove_user(username)? == 0 {
+fn remove(username: String, dir: Option<String>) -> Result<()> {
+    let repo = open_config_repo(dir.as_deref())?;
+    if repo.remove_user(&username)? == 0 {
         return Err(anyhow!("user does not exist"));
     }
     println!("User removed: username=\"{username}\"");
     Ok(())
 }
 
-fn password(args: &clap::ArgMatches) -> Result<()> {
-    let username = args.get_one::<String>("username").unwrap();
-    let repo = open_config_repo(args.get_one::<String>("data-directory").map(|s| &**s))?;
-    let user = repo.get_user_by_name(username)?;
+fn password(username: String, dir: Option<String>) -> Result<()> {
+    let repo = open_config_repo(dir.as_deref())?;
+    let user = repo.get_user_by_name(&username)?;
     let password = rpassword::read_password_from_tty(Some("Password: "))?;
     if password.is_empty() {
         return Err(anyhow!("empty password not allowed"));
