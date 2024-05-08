5fa4e059621c11608fe423f90909b445c384339d
Jason Ish <jason@codemonkey.net>
2024-05-08 15:09:41 -0600

    github-ci: fix job; update actions

--- build-dist.sh
+++ build-dist.sh
@@ -23,21 +23,17 @@ for a in $@; do
         --linux)
             skip_windows="yes"
             ;;
-	--*)
-	    echo "error: bad argument: $a"
-	    exit 1
-	    ;;
+	      --*)
+	          echo "error: bad argument: $a"
+	          exit 1
+	          ;;
         *)
-	    command="$@"
-	    break
+	          command="$@"
+	          break
             ;;
     esac
     shift
 done
-
-if test -t; then
-    it="-it"
-fi
 
 cross_run() {
     target="$1"
@@ -49,20 +45,20 @@ cross_run() {
     dockerfile="./docker/builder/Dockerfile.cross"
     tag=${BUILDER_TAG:-"evebox/builder:cross"}
     ${ECHO} docker build \
-        --build-arg REAL_UID="$(id -u)" \
-        --build-arg REAL_GID="$(id -g)" \
-        --cache-from ${tag} \
-	-t ${tag} \
-	-f ${dockerfile} .
-    ${ECHO} docker run --rm ${it} --privileged \
-        -v "$(pwd):/src:z" \
-        -v /var/run/docker.sock:/var/run/docker.sock:z \
-        -w /src \
-        -e BUILD_REV="${BUILD_REV}" \
-        -e TARGET="${target}" \
-        -u builder \
-        --group-add $(getent group docker | cut -f3 -d:) \
-        ${tag} $@
+            --build-arg REAL_UID="$(id -u)" \
+            --build-arg REAL_GID="$(id -g)" \
+            --cache-from ${tag} \
+	          -t ${tag} \
+	          -f ${dockerfile} .
+    ${ECHO} docker run --rm --privileged \
+            -v "$(pwd):/src:z" \
+            -v /var/run/docker.sock:/var/run/docker.sock:z \
+            -w /src \
+            -e BUILD_REV="${BUILD_REV}" \
+            -e TARGET="${target}" \
+            -u builder \
+            --group-add $(getent group docker | cut -f3 -d:) \
+            ${tag} $@
 }
 
 if [[ "${command}" ]]; then
@@ -70,13 +66,13 @@ if [[ "${command}" ]]; then
 else
     cross_run x86_64-unknown-linux-musl make dist
     cross_run aarch64-unknown-linux-musl make dist
-
+    
     cross_run x86_64-unknown-linux-musl ./packaging/build-rpm.sh amd64
-
+    
     cross_run x86_64-unknown-linux-musl ./packaging/build-deb.sh amd64
     cross_run aarch64-unknown-linux-musl ./packaging/build-deb.sh arm64
-
+    
     if [[ "${skip_windows}" != "yes" ]]; then
-	cross_run x86_64-pc-windows-gnu make dist
+	      cross_run x86_64-pc-windows-gnu make dist
     fi
 fi
