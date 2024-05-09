496ce1ed22517c79266b3e699cad16ba6615d575
Jason Ish <jason@codemonkey.net>
2024-05-09 09:31:50 -0600

    sqlite: add api endpoint to check fts integrity

--- src/server/api/sqlite.rs
+++ src/server/api/sqlite.rs
@@ -4,37 +4,38 @@
 use crate::{
     eventrepo::EventRepo,
     server::{main::SessionExtractor, ServerContext},
-    sqlite::info::Info,
+    sqlite::{self, info::Info},
 };
 use axum::{response::IntoResponse, Extension, Json};
 use serde::Serialize;
 use std::sync::Arc;
+use tracing::info;
 
 use super::ApiError;
-
-#[derive(Default, Serialize)]
-struct InfoResponse {
-    auto_vacuum: u8,
-    journal_mode: String,
-    synchronous: u8,
-    fts_enabled: bool,
-    page_size: i64,
-    page_count: i64,
-    freelist_count: i64,
-    min_event_id: u64,
-    max_event_id: u64,
-    event_count_estimate: u64,
-    data_size: u64,
-    schema_version: u64,
-    min_timestamp: Option<String>,
-    max_timestamp: Option<String>,
-}
 
 pub(crate) async fn info(
     context: Extension<Arc<ServerContext>>,
     _session: SessionExtractor,
 ) -> Result<impl IntoResponse, ApiError> {
     if let EventRepo::SQLite(sqlite) = &context.datastore {
+        #[derive(Default, Serialize)]
+        struct Response {
+            auto_vacuum: u8,
+            journal_mode: String,
+            synchronous: u8,
+            fts_enabled: bool,
+            page_size: i64,
+            page_count: i64,
+            freelist_count: i64,
+            min_event_id: u64,
+            max_event_id: u64,
+            event_count_estimate: u64,
+            data_size: u64,
+            schema_version: u64,
+            min_timestamp: Option<String>,
+            max_timestamp: Option<String>,
+        }
+
         let min_row_id = sqlite.min_row_id().await?;
         let max_row_id = sqlite.max_row_id().await?;
         let event_count_estimate = max_row_id - min_row_id;
@@ -46,8 +47,8 @@ pub(crate) async fn info(
             .pool
             .get()
             .await?
-            .interact(move |conn| -> Result<InfoResponse, rusqlite::Error> {
-                let mut response = InfoResponse {
+            .interact(move |conn| -> Result<Response, rusqlite::Error> {
+                let mut response = Response {
                     min_timestamp: min_timestamp.map(|ts| ts.to_string()),
                     max_timestamp: max_timestamp.map(|ts| ts.to_string()),
                     ..Default::default()
@@ -66,6 +67,45 @@ pub(crate) async fn info(
                 response.data_size = (response.page_size * response.page_count) as u64;
                 response.schema_version = info.schema_version()?;
                 response.event_count_estimate = event_count_estimate;
+                Ok(response)
+            })
+            .await??;
+        return Ok(Json(response).into_response());
+    }
+
+    Ok(().into_response())
+}
+
+pub(crate) async fn fts_check(
+    context: Extension<Arc<ServerContext>>,
+    _session: SessionExtractor,
+) -> Result<impl IntoResponse, ApiError> {
+    if let EventRepo::SQLite(sqlite) = &context.datastore {
+        #[derive(Debug, Serialize)]
+        struct Response {
+            ok: bool,
+            #[serde(skip_serializing_if = "Option::is_none")]
+            error: Option<String>,
+        }
+
+        info!("Running SQLite FTS integrity check from API");
+
+        let response = sqlite
+            .pool
+            .get()
+            .await?
+            .interact(move |conn| -> Result<Response, rusqlite::Error> {
+                let response = match sqlite::util::fts_check(conn) {
+                    Ok(_) => Response {
+                        ok: true,
+                        error: None,
+                    },
+                    Err(err) => Response {
+                        ok: false,
+                        error: Some(format!("{:?}", err)),
+                    },
+                };
+
                 Ok(response)
             })
             .await??;
