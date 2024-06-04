jasonish/evebox

commit 7f9b9bdd84eb2b99b344ba7998872cbe7bde50af

api: reduce data between server and client
For the alerts view, don't send the while record from the database,
just the fields to needed to present the alert view.

Reduces data by 80% or so.

When clicking into an event, the event will need to be loaded from the
server now.

src/elastic/eventrepo/alerts.rs
@@ -1,0 +1,218 @@
+// SPDX-FileCopyrightText: (C) 2020 Jason Ish <jason@codemonkey.net>
+// SPDX-License-Identifier: MIT
+
+use axum::{response::IntoResponse, Json};
+use tracing::{error, info, warn};
+
+use crate::{
+    elastic::{AlertQueryOptions, ElasticResponse},
+    eventrepo::DatastoreError,
+    queryparser::{self, QueryValue},
+};
+
+use super::{ElasticEventRepo, MINIMUM_SHOULD_MATCH};
+
+impl ElasticEventRepo {
+    pub fn build_inbox_query(&self, options: AlertQueryOptions) -> serde_json::Value {
+        let mut filters = Vec::new();
+        let mut should = Vec::new();
+        let mut must_not = Vec::new();
+
+        // Set to true if the min timestamp is set in the query string
+        let mut has_min_timestamp = false;
+
+        if let Some(q) = &options.query_string {
+            // TODO: Need client tz_offset here.
+            match queryparser::parse(q, None) {
+                Ok(elements) => {
+                    self.apply_query_string(&elements, &mut filters, &mut should, &mut must_not);
+                    has_min_timestamp = elements
+                        .iter()
+                        .any(|e| matches!(&e.value, QueryValue::From(_)));
+                }
+                Err(err) => {
+                    error!(
+                        "Failed to parse query string: error={}, query-string={}",
+                        q, err
+                    );
+                }
+            }
+        }
+
+        filters.push(json!({"exists": {"field": self.map_field("event_type")}}));
+        filters.push(json!({"term": {self.map_field("event_type"): "alert"}}));
+
+        if let Some(sensor) = &options.sensor {
+            filters.push(json!({"term": {self.map_field("host"): sensor}}));
+        }
+
+        if !has_min_timestamp {
+            if let Some(ts) = options.timestamp_gte {
+                filters.push(json!({"range": {"@timestamp": {"gte": ts.to_elastic()}}}));
+            }
+        }
+
+        for tag in options.tags {
+            if let Some(tag) = tag.strip_prefix('-') {
+                let j = json!({"term": {"tags": tag}});
+                must_not.push(j);
+            } else {
+                let j = json!({"term": {"tags": tag}});
+                filters.push(j);
+            }
+        }
+
+        // Reducing the source size from ECS is not done yet.
+        let source = if self.ecs {
+            json!([])
+        } else {
+            json!([
+                "alert.action",
+                "alert.severity",
+                "alert.signature",
+                "alert.signature_id",
+                "app_proto",
+                "dest_ip",
+                "src_ip",
+                "tags",
+                "timestamp",
+            ])
+        };
+
+        let mut query = json!({
+            "query": {
+                "bool": {
+                    "filter": filters,
+                    "must_not": must_not,
+                }
+            },
+            "sort": [{"@timestamp": {"order": "desc"}}],
+            "aggs": {
+                "signatures": {
+                    "terms": {"field": self.map_field("alert.signature_id"), "size": 2000},
+                    "aggs": {
+                        "sources": {
+                            "terms": {"field": self.map_field("src_ip"), "size": 1000},
+                            "aggs": {
+                                "destinations": {
+                                    "terms": {
+                                        "field": self.map_field("dest_ip"),
+                                        "size": 500
+                                    },
+                                    "aggs": {
+                                        "escalated": {
+                                            "filter": {
+                                                "term": {
+                                                    "tags": "evebox.escalated",
+                                                }
+                                            }
+                                        },
+                                        "newest": {
+                                            "top_hits": {
+                                                "size": 1,
+                                                "sort": [
+                                                    {
+                                                        self.map_field("timestamp"): {"order": "desc"}
+                                                    }
+                                                ],
+                                                "_source": source,
+                                            }
+                                        },
+                                        "oldest": {
+                                            "top_hits": {
+                                                "size": 1,
+                                                "sort": [
+                                                    {
+                                                        self.map_field("timestamp"): {"order": "asc"}
+                                                    }
+                                                ],
+                                                // We only need the
+                                                // timestamp from the
+                                                // oldest event.
+                                                "_source": [
+                                                    "timestamp",
+                                                ]
+                                            }
+                                        }
+                                    },
+                                },
+                            },
+                        },
+                    },
+                }
+            }
+        });
+
+        if !should.is_empty() {
+            query["query"]["bool"]["should"] = should.into();
+            query["query"]["bool"][MINIMUM_SHOULD_MATCH] = 1.into();
+        }
+
+        query
+    }
+
+    pub async fn alerts(
+        &self,
+        options: AlertQueryOptions,
+    ) -> Result<impl IntoResponse, DatastoreError> {
+        let query = self.build_inbox_query(options);
+        let start = std::time::Instant::now();
+        let body = self.search(&query).await?.text().await?;
+        let response: ElasticResponse = serde_json::from_str(&body)?;
+        if let Some(error) = response.error {
+            return Err(DatastoreError::ElasticSearchError(error.first_reason()));
+        }
+
+        info!(
+            "Elasticsearch alert query took {:?}, es-time: {}, response-size: {}",
+            start.elapsed(),
+            response.took,
+            body.len()
+        );
+
+        let mut alerts = Vec::new();
+        if let Some(aggregrations) = response.aggregations {
+            if let serde_json::Value::Array(buckets) = &aggregrations["signatures"]["buckets"] {
+                for bucket in buckets {
+                    if let serde_json::Value::Array(buckets) = &bucket["sources"]["buckets"] {
+                        for bucket in buckets {
+                            if let serde_json::Value::Array(buckets) =
+                                &bucket["destinations"]["buckets"]
+                            {
+                                for bucket in buckets {
+                                    let mut newest = bucket["newest"]["hits"]["hits"][0].clone();
+                                    let mut oldest = bucket["oldest"]["hits"]["hits"][0].clone();
+
+                                    if self.ecs {
+                                        self.transform_ecs(&mut newest);
+                                        self.transform_ecs(&mut oldest);
+                                    }
+
+                                    let escalated = &bucket["escalated"]["doc_count"];
+
+                                    newest["_metadata"] = json!({
+                                        "count": bucket["doc_count"],
+                                        "escalated_count": escalated,
+                                        "min_timestamp": &oldest["_source"]["timestamp"],
+                                        "max_timestamp": &newest["_source"]["timestamp"],
+                                        "aggregate": true,
+                                    });
+                                    alerts.push(newest);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            warn!("Elasticsearch response has no aggregations");
+        }
+
+        let response = json!({
+            "ecs": self.ecs,
+            "events": alerts,
+        });
+
+        Ok(Json(response))
+    }
+}

src/sqlite/eventrepo/alerts.rs
@@ -12,6 +12,7 @@ use tracing::{debug, error, info, instrument, warn};
 use super::SqliteEventRepo;
 use crate::datetime::DateTime;
 use crate::server::api::{ApiAlertGroup, ApiAlertGroupMetadata, ApiAlertResponse};
+use crate::sqlite::builder::EventQueryBuilder;
 use crate::sqlite::log_query_plan;
 use crate::{elastic::AlertQueryOptions, eventrepo::DatastoreError};
 use crate::{queryparser, LOG_QUERIES, LOG_QUERY_PLAN};
@@ -34,45 +35,47 @@ impl SqliteEventRepo {
             max_timestamp: u64,
         }
 
-        let mut filters: Vec<String> = Vec::new();
-        let mut args = SqliteArguments::default();
+        let mut builder = EventQueryBuilder::new(self.fts().await);
+        builder
+            .select("rowid")
+            .select("timestamp")
+            .select("escalated")
+            .select("archived")
+            .selectjs("alert.signature_id")
+            .selectjs("alert.signature")
+            .selectjs("alert.severity")
+            .selectjs("alert.action")
+            .selectjs("app_proto")
+            .selectjs("dest_ip")
+            .selectjs("src_ip")
+            .selectjs("tags");
+        builder.from("events");
+        builder.order_by("timestamp", "DESC");
 
-        let sql = r#"
-            SELECT rowid, timestamp, escalated, archived, source,
-                json_extract(events.source, '$.alert.signature_id') as sid
-            FROM events
-            WHERE %WHERE%
-            ORDER BY timestamp DESC"#;
-
-        filters.push("json_extract(events.source, '$.event_type') = 'alert'".to_string());
+        builder.wherejs("event_type", "=", "alert");
 
         for tag in options.tags {
             match tag.as_ref() {
                 "evebox.archived" => {
-                    filters.push("archived = ?".into());
-                    args.add(1);
+                    builder.push_where("archived = ?").push_arg(1);
                 }
                 "-evebox.archived" => {
-                    filters.push("archived = ?".into());
-                    args.add(0);
+                    builder.push_where("archived = ?").push_arg(0);
                 }
                 "evebox.escalated" => {
-                    filters.push("escalated = ?".into());
-                    args.add(1);
+                    builder.push_where("escalated = ?").push_arg(1);
                 }
                 _ => {}
             }
         }
 
         if let Some(sensor) = options.sensor {
-            filters.push("json_extract(events.source, '$.host') = ?".into());
-            args.add(sensor);
+            builder.wherejs("host", "=", sensor);
         }
 
         // TODO: With a timeout, we can remove this.
         if let Some(ts) = options.timestamp_gte {
-            filters.push("timestamp >= ?".into());
-            args.add(ts.to_nanos());
+            builder.push_where("timestamp >= ?").push_arg(ts.to_nanos());
         }
 
         // Query string.
@@ -89,24 +92,21 @@ impl SqliteEventRepo {
                         match &el.value {
                             queryparser::QueryValue::String(s) => {
                                 if el.negated {
-                                    filters.push("events.source NOT LIKE ?".into());
-                                    args.add(format!("%{s}%"));
+                                    builder
+                                        .push_where("events.source NOT LIKE ?")
+                                        .push_arg(format!("%{}%", s));
                                 } else {
-                                    filters.push("events.source LIKE ?".into());
-                                    args.add(format!("%{s}%"));
+                                    builder
+                                        .push_where("events.source LIKE ?")
+                                        .push_arg(format!("%{}%", s));
                                 }
                             }
                             queryparser::QueryValue::KeyValue(k, v) => {
                                 // TODO: Handle negation - maybe use query builder?
                                 if let Ok(v) = v.parse::<i64>() {
-                                    filters
-                                        .push(format!("json_extract(events.source, '$.{k}') = ?"));
-                                    args.add(v);
+                                    builder.wherejs(k, "=", v);
                                 } else {
-                                    filters.push(format!(
-                                        "json_extract(events.source, '$.{k}') LIKE ?"
-                                    ));
-                                    args.add(format!("%{v}%"));
+                                    builder.wherejs(k, "LIKE", format!("%{}%", v));
                                 }
                             }
                             queryparser::QueryValue::From(_) => {
@@ -121,10 +121,10 @@ impl SqliteEventRepo {
             }
         }
 
-        let sql = sql.replace("%WHERE%", &filters.join(" AND "));
+        let (sql, args) = builder.build();
 
         if *LOG_QUERY_PLAN {
-            log_query_plan(&self.pool, &sql, &SqliteArguments::default()).await;
+            log_query_plan(&self.pool, &sql, &args).await;
         } else if *LOG_QUERIES {
             info!(
                 "query={}; args={:?}",
@@ -139,17 +139,34 @@ impl SqliteEventRepo {
         let mut timed_out = false;
         let mut count = 0;
         while let Some(row) = rows.try_next().await? {
-            let rowid: u64 = row.try_get(0)?;
-            let timestamp: u64 = row.try_get(1)?;
-            let escalated: bool = row.try_get(2)?;
-            let archived: bool = row.try_get(3)?;
-            let mut source: serde_json::Value = row.try_get(4)?;
-            let sid: u64 = row.try_get(5)?;
+            let rowid: u64 = row.try_get("rowid")?;
+            let timestamp: u64 = row.try_get("timestamp")?;
+            let escalated: bool = row.try_get("escalated")?;
+            let archived: bool = row.try_get("archived")?;
+            let alert_signature_id: u64 = row.try_get("alert.signature_id")?;
+            let alert_signature: String = row.try_get("alert.signature")?;
+            let alert_severity: u64 = row.try_get("alert.severity")?;
+            let alert_action: String = row.try_get("alert.action")?;
+            let app_proto: String = row.try_get("app_proto")?;
+            let dest_ip: String = row.try_get("dest_ip")?;
+            let src_ip: String = row.try_get("src_ip")?;
+            let tags: serde_json::Value = row.try_get("tags").unwrap_or(serde_json::Value::Null);
 
-            let src_ip = source["src_ip"].as_str().unwrap().to_string();
-            let dest_ip = source["dest_ip"].as_str().unwrap().to_string();
+            let mut source = json!({
+                "timestamp": DateTime::from_nanos(timestamp as i64).to_eve(),
+                "tags": tags,
+                "dest_ip": dest_ip,
+                "src_ip": src_ip,
+                "app_proto": app_proto,
+                "alert": {
+                    "signature": alert_signature,
+                    "signature_id": alert_signature_id,
+                    "severity": alert_severity,
+                    "action": alert_action,
+                },
+            });
 
-            let key = format!("{sid}{src_ip}{dest_ip}");
+            let key = format!("{alert_signature_id}{src_ip}{dest_ip}");
 
             if let serde_json::Value::Null = &source["tags"] {
                 let tags: Vec<String> = Vec::new();
@@ -380,11 +397,23 @@ fn alert_row_mapper(row: SqliteRow) -> Result<ApiAlertGroup, DatastoreError> {
     }
 
     let min_ts = DateTime::from_nanos(min_ts_nanos);
-    let max_ts = crate::datetime::parse(&parsed["timestamp"].as_str().unwrap(), None)?;
+    let max_ts = crate::datetime::parse(parsed["timestamp"].as_str().unwrap(), None)?;
 
     let alert = ApiAlertGroup {
         id: id.to_string(),
-        source: parsed,
+        source: json!({
+            "alert": {
+                "action": parsed["alert"]["action"],
+                "severity": parsed["alert"]["severity"],
+                "signature": parsed["alert"]["signature"],
+                "signature_id": parsed["alert"]["signature_id"],
+            },
+            "app_proto": parsed["app_proto"],
+            "dest_ip": parsed["dest_ip"],
+            "src_ip": parsed["src_ip"],
+            "tags": parsed["tags"],
+            "timestamp": parsed["timestamp"],
+        }),
         metadata: ApiAlertGroupMetadata {
             count: count as u64,
             escalated_count: escalated_count as u64,

src/elastic/eventrepo.rs
@@ -13,20 +13,17 @@ use crate::datetime::DateTime;
 use crate::elastic::importer::ElasticEventSink;
 use crate::elastic::request::exists_filter;
 use crate::elastic::{
-    request, AlertQueryOptions, ElasticResponse, ACTION_DEESCALATED, ACTION_ESCALATED,
-    TAGS_ARCHIVED, TAGS_ESCALATED, TAG_ARCHIVED,
+    request, ElasticResponse, ACTION_DEESCALATED, ACTION_ESCALATED, TAGS_ARCHIVED, TAGS_ESCALATED,
+    TAG_ARCHIVED,
 };
 use crate::eventrepo::{self, DatastoreError};
 use crate::queryparser;
 use crate::queryparser::QueryElement;
 use crate::queryparser::QueryParser;
-use crate::queryparser::QueryValue;
 use crate::server::api;
 use crate::server::session::Session;
 use crate::util;
 use crate::LOG_QUERIES;
-use axum::response::IntoResponse;
-use axum::Json;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::sync::Arc;
@@ -35,6 +32,7 @@ use tracing::error;
 use tracing::info;
 use tracing::warn;
 
+mod alerts;
 mod dhcp;
 mod stats;
 
@@ -467,190 +465,6 @@ impl ElasticEventRepo {
                 }
             }
         }
-    }
-
-    pub fn build_inbox_query(&self, options: AlertQueryOptions) -> serde_json::Value {
-        let mut filters = Vec::new();
-        let mut should = Vec::new();
-        let mut must_not = Vec::new();
-
-        // Set to true if the min timestamp is set in the query string
-        let mut has_min_timestamp = false;
-
-        if let Some(q) = &options.query_string {
-            // TODO: Need client tz_offset here.
-            match queryparser::parse(q, None) {
-                Ok(elements) => {
-                    self.apply_query_string(&elements, &mut filters, &mut should, &mut must_not);
-                    has_min_timestamp = elements
-                        .iter()
-                        .any(|e| matches!(&e.value, QueryValue::From(_)));
-                }
-                Err(err) => {
-                    error!(
-                        "Failed to parse query string: error={}, query-string={}",
-                        q, err
-                    );
-                }
-            }
-        }
-
-        filters.push(json!({"exists": {"field": self.map_field("event_type")}}));
-        filters.push(json!({"term": {self.map_field("event_type"): "alert"}}));
-
-        if let Some(sensor) = &options.sensor {
-            filters.push(json!({"term": {self.map_field("host"): sensor}}));
-        }
-
-        if !has_min_timestamp {
-            if let Some(ts) = options.timestamp_gte {
-                filters.push(json!({"range": {"@timestamp": {"gte": ts.to_elastic()}}}));
-            }
-        }
-
-        for tag in options.tags {
-            if let Some(tag) = tag.strip_prefix('-') {
-                let j = json!({"term": {"tags": tag}});
-                must_not.push(j);
-            } else {
-                let j = json!({"term": {"tags": tag}});
-                filters.push(j);
-            }
-        }
-
-        let mut query = json!({
-            "query": {
-                "bool": {
-                    "filter": filters,
-                    "must_not": must_not,
-                }
-            },
-            "sort": [{"@timestamp": {"order": "desc"}}],
-            "aggs": {
-                "signatures": {
-                    "terms": {"field": self.map_field("alert.signature_id"), "size": 2000},
-                    "aggs": {
-                        "sources": {
-                            "terms": {"field": self.map_field("src_ip"), "size": 1000},
-                            "aggs": {
-                                "destinations": {
-                                    "terms": {
-                                        "field": self.map_field("dest_ip"),
-                                        "size": 500
-                                    },
-                                    "aggs": {
-                                        "escalated": {
-                                            "filter": {
-                                                "term": {
-                                                    "tags": "evebox.escalated"}
-                                            }
-                                        },
-                                        "newest": {
-                                            "top_hits": {
-                                                "size": 1,
-                                                "sort": [
-                                                    {
-                                                        self.map_field("timestamp"): {"order": "desc"}
-                                                    }
-                                                ]
-                                            }
-                                        },
-                                        "oldest": {
-                                            "top_hits": {
-                                                "size": 1,
-                                                "sort": [
-                                                    {
-                                                        self.map_field("timestamp"): {"order": "asc"}
-                                                    }
-                                                ],
-                                                // We only need the
-                                                // timestamp from the
-                                                // oldest event.
-                                                "_source": [
-                                                    "timestamp",
-                                                ]
-                                            }
-                                        }
-                                    },
-                                },
-                            },
-                        },
-                    },
-                }
-            }
-        });
-
-        if !should.is_empty() {
-            query["query"]["bool"]["should"] = should.into();
-            query["query"]["bool"][MINIMUM_SHOULD_MATCH] = 1.into();
-        }
-
-        query
-    }
-
-    pub async fn alerts(
-        &self,
-        options: AlertQueryOptions,
-    ) -> Result<impl IntoResponse, DatastoreError> {
-        let query = self.build_inbox_query(options);
-        let start = std::time::Instant::now();
-        let body = self.search(&query).await?.text().await?;
-        let response: ElasticResponse = serde_json::from_str(&body)?;
-        if let Some(error) = response.error {
-            return Err(DatastoreError::ElasticSearchError(error.first_reason()));
-        }
-
-        info!(
-            "Elasticsearch alert query took {:?}, es-time: {}, response-size: {}",
-            start.elapsed(),
-            response.took,
-            body.len()
-        );
-
-        let mut alerts = Vec::new();
-        if let Some(aggregrations) = response.aggregations {
-            if let serde_json::Value::Array(buckets) = &aggregrations["signatures"]["buckets"] {
-                for bucket in buckets {
-                    if let serde_json::Value::Array(buckets) = &bucket["sources"]["buckets"] {
-                        for bucket in buckets {
-                            if let serde_json::Value::Array(buckets) =
-                                &bucket["destinations"]["buckets"]
-                            {
-                                for bucket in buckets {
-                                    let mut newest = bucket["newest"]["hits"]["hits"][0].clone();
-                                    let mut oldest = bucket["oldest"]["hits"]["hits"][0].clone();
-
-                                    if self.ecs {
-                                        self.transform_ecs(&mut newest);
-                                        self.transform_ecs(&mut oldest);
-                                    }
-
-                                    let escalated = &bucket["escalated"]["doc_count"];
-
-                                    newest["_metadata"] = json!({
-                                        "count": bucket["doc_count"],
-                                        "escalated_count": escalated,
-                                        "min_timestamp": &oldest["_source"]["timestamp"],
-                                        "max_timestamp": &newest["_source"]["timestamp"],
-                                        "aggregate": true,
-                                    });
-                                    alerts.push(newest);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        } else {
-            warn!("Elasticsearch response has no aggregations");
-        }
-
-        let response = json!({
-            "ecs": self.ecs,
-            "events": alerts,
-        });
-
-        Ok(Json(response))
     }
 
     fn transform_ecs(&self, event: &mut serde_json::Value) {

commit b2b4530aa6162c2c662db60604ab3d750eba10ac

elastic/ecs: fix min timestamp in aggregate alerts
Fixes: 7f9b9bdd84eb2b99b344ba7998872cbe7bde50af

src/elastic/eventrepo/alerts.rs
@@ -131,6 +131,9 @@ impl ElasticEventRepo {
                                                 // oldest event.
                                                 "_source": [
                                                     "timestamp",
+
+                                                    // ECS doesn't have timestamp.
+                                                    "@timestamp",
                                                 ]
                                             }
                                         }
@@ -181,19 +184,25 @@ impl ElasticEventRepo {
                             {
                                 for bucket in buckets {
                                     let mut newest = bucket["newest"]["hits"]["hits"][0].clone();
-                                    let mut oldest = bucket["oldest"]["hits"]["hits"][0].clone();
+                                    let oldest = bucket["oldest"]["hits"]["hits"][0].clone();
 
                                     if self.ecs {
                                         self.transform_ecs(&mut newest);
-                                        self.transform_ecs(&mut oldest);
                                     }
 
                                     let escalated = &bucket["escalated"]["doc_count"];
 
+                                    let min_timestamp =
+                                        if oldest["_source"]["timestamp"].is_string() {
+                                            &oldest["_source"]["timestamp"]
+                                        } else {
+                                            &oldest["_source"]["@timestamp"]
+                                        };
+
                                     newest["_metadata"] = json!({
                                         "count": bucket["doc_count"],
                                         "escalated_count": escalated,
-                                        "min_timestamp": &oldest["_source"]["timestamp"],
+                                        "min_timestamp": min_timestamp,
                                         "max_timestamp": &newest["_source"]["timestamp"],
                                         "aggregate": true,
                                     });