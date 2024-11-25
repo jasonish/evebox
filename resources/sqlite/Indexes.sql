e737f7eed9367f2f25a44def961c37bd25650dd4
Jason Ish <jason@codemonkey.net>
2024-11-25 16:55:41 -0600

    sqlite/indexes: cleanup with some notes
    No functional changes, but:
    - add changelog
    - remove drops, handled by code instead

--- resources/sqlite/Indexes.sql
+++ resources/sqlite/Indexes.sql
@@ -1,63 +1,49 @@
--- Drop indexes no longer used.
-DROP INDEX IF EXISTS events_event_type_archived;
-DROP INDEX IF EXISTS events_escalated_view_index;
-DROP INDEX IF EXISTS events_event_type_index;
-DROP INDEX IF EXISTS events_archived_index;
-DROP INDEX IF EXISTS events_escalated_index;
-DROP INDEX IF EXISTS events_src_ip_index;
-DROP INDEX IF EXISTS events_dest_ip_index;
-
+-- Existed in 0.18.
 CREATE INDEX IF NOT EXISTS events_timestamp_index
   ON events (
     timestamp
 );
 
+-- Existed in 0.18.
 CREATE INDEX IF NOT EXISTS events_alert_signature_index
   ON events (
     json_extract(source, '$.alert.signature')
 );
 
+-- Existed in 0.18.
 CREATE INDEX IF NOT EXISTS events_alert_signature_id_index
   ON events (
     json_extract(source, '$.alert.signature_id')
 );
 
+-- Existed in 0.18.
 CREATE INDEX IF NOT EXISTS events_flow_id_index
   ON events (
     json_extract(source, '$.flow_id')
 );
 
--- Slow when fetching non-archived events one by one in descending
--- timestamp order. And can't detect when we've hit the last
--- non-archived event.
---
--- Replaced by: index_events_event_type_v2
--- Removed in: 0.19
--- CREATE INDEX events_event_type_timestamp_index_v1
---   ON events (
---     json_extract(source, '$.event_type'),
---     timestamp
--- );
-
--- Should be able to drop this one.
+-- Existed in 0.18.
 CREATE INDEX IF NOT EXISTS events_escalated_v1
   ON events (
     escalated,
     timestamp
 );
 
+-- Existed in 0.18.
 CREATE INDEX IF NOT EXISTS events_src_ip_index_v1
   ON events (
     json_extract(source, '$.src_ip'),
     timestamp DESC
 );
 
+-- Existed in 0.18.
 CREATE INDEX IF NOT EXISTS events_dest_ip_index_v1
   ON events (
     json_extract(source, '$.dest_ip'),
     timestamp DESC
 );
 
+-- Existed in 0.18.
 CREATE INDEX IF NOT EXISTS events_archive_index_v1
   ON events (
     archived,
@@ -68,24 +54,27 @@ CREATE INDEX IF NOT EXISTS events_archive_index_v1
   WHERE json_extract(source, '$.event_type') = 'alert'
     AND archived = 0;
 
--- Fast query of distinct host names.
--- Fast query of events by sensor.
-CREATE INDEX events_sensors_timestamp_index_v1 on events (
-  json_extract(source, '$.host'),
-  json_extract(source, '$.event_type'),
-  timestamp
+-- Existed in 0.18.
+CREATE INDEX IF NOT EXISTS events_sensors_timestamp_index_v1
+  ON events (
+    json_extract(source, '$.host'),
+    json_extract(source, '$.event_type'),
+    timestamp
 );
 
----
---- 0.19
----
-
+-- New in 0.19.
 -- Replaces: events_event_type_timestamp_index_v1
-DROP INDEX IF EXISTS events_event_type_timestamp_index_v1;
-CREATE INDEX index_events_event_type_v2
+CREATE INDEX IF NOT EXISTS index_events_event_type_v2
   ON events (
     json_extract(source, '$.event_type'), 
     timestamp,
     archived,
     escalated
 );
+
+--
+-- Changelog
+--
+-- 0.19.0
+-- - Drop: events_event_type_timestamp_index_v1
+-- - Add: index_events_event_type_v2
