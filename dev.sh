11fa55a00a753b9fa0fd871560a9243ef7a8d7c8
Jason Ish <jason@codemonkey.net>
2024-12-13 11:53:31 -0600

    dev.sh: enable rust backtraces

--- dev.sh
+++ dev.sh
@@ -11,5 +11,7 @@ if ! test -d webapp/node_modules; then
     (cd webapp && npm ci --prefer-offline)
 fi
 
+export RUST_BACKTRACE=1
+
 (cd webapp && npm start) &
 cargo watch -w src -i webapp -x "run ${RELEASE} server --no-tls --no-auth ${args}"
