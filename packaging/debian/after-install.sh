d7862bf57533399f71c4e2a6ac6682fb8b7c14c3
Jason Ish <jason@codemonkey.net>
2025-07-26 00:04:10 -0600

    packaging: create /var/lib/evebox directory in debian after-install script
    The debian package creates the evebox user with /var/lib/evebox as home
    directory but doesn't actually create the directory. This causes issues
    when the evebox services try to use EVEBOX_DATA_DIRECTORY=/var/lib/evebox.
    
    Fixes #346

--- packaging/debian/after-install.sh
+++ packaging/debian/after-install.sh
@@ -13,3 +13,10 @@ if ! /usr/bin/getent passwd ${USERNAME} > /dev/null; then
             --shell /usr/sbin/nologin \
             ${USERNAME}
 fi
+
+# Create the data directory if it doesn't exist
+if [ ! -d ${HOMEDIR} ]; then
+    mkdir -p ${HOMEDIR}
+    chown ${USERNAME}:${USERNAME} ${HOMEDIR}
+    chmod 750 ${HOMEDIR}
+fi
